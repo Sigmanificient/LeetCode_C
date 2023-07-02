b56670d17be175f764ac37101ecc3b6f1eb4a962
Sigmanificient <edhyjox@gmail.com>
2023-07-02 21:50:39 +0200

    :white_check_mark: Add solution for 2 sum

--- src/0003-longest-substring-without-repeating-characters/longest_substring.c
+++ src/0003-longest-substring-without-repeating-characters/longest_substring.c
@@ -1,10 +1,50 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX(x, y) ((x > y) ? (x) : (y))
+
 #define ATTR(key) __attribute__((key))
 #define ALIAS(name) ATTR(alias(name))
 #define USED ATTR(used)
 
-int length_of_longest_substring(char * s)
+enum { SENTINEL = -1 };
+
+int uniq_chain(const char *str, size_t len, char *pool)
 {
-    return 0;
+    bool found;
+    int count = 0;
+    size_t pool_last = 0;
+
+    memset(pool, 0, len);
+    for (size_t i = 0; str[i]; i++) {
+        found = false;
+        for (size_t j = 0; j < pool_last; j++) {
+            if (pool[j] == str[i]) {
+                found = true;
+                break;
+            }
+        }
+        if (found)
+            return count;
+        pool[pool_last++] = str[i];
+        count++;
+    }
+    return count;
+}
+
+int length_of_longest_substring(char *str)
+{
+    int count = 0;
+    size_t len = strlen(str);
+    char *pool = malloc(len * sizeof (char));
+
+    if (pool == NULL)
+        return SENTINEL;
+    for (size_t i = 0; i < len; i++)
+        count = MAX(count, uniq_chain(str + i, len - i, pool));
+    free(pool);
+    return count;
 }
 
 int lengthOfLongestSubstring() USED ALIAS("length_of_longest_substring");
--- src/0003-longest-substring-without-repeating-characters/tests.c
+++ src/0003-longest-substring-without-repeating-characters/tests.c
@@ -1,19 +1,32 @@
 #include <stdlib.h>
 #include <criterion/criterion.h>
 
+enum { SENTINEL = -1 };
 int length_of_longest_substring(char * s);
 
 Test(longest_substring, test_case_1)
 {
-    cr_assert_eq(length_of_longest_substring("abcabcbb"), 3);
+    int actual = length_of_longest_substring("abcabcbb");
+
+    if (actual == SENTINEL)
+        cr_skip("Allocation Failure.");
+    cr_assert_eq(actual, 3, "actual:%d", actual);
 }
 
 Test(longest_substring, test_case_2)
 {
-    cr_assert_eq(length_of_longest_substring("bbbbb"), 1);
+    int actual = length_of_longest_substring("bbbbb");
+
+    if (actual == SENTINEL)
+        cr_skip("Allocation Failure.");
+    cr_assert_eq(actual, 1, "actual:%d", actual);
 }
 
 Test(longest_substring, test_case_3)
 {
-    cr_assert_eq(length_of_longest_substring("pwwkew"), 3);
+    int actual = length_of_longest_substring("pwwkew");
+
+    if (actual == SENTINEL)
+        cr_skip("Allocation Failure.");
+    cr_assert_eq(actual, 3, "actual:%d", actual);
 }
