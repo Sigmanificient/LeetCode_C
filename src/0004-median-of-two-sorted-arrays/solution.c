c8df8329e8f5587504f5f5644e93e936ef71d179
Sigmanificient <edhyjox@gmail.com>
2023-07-05 12:18:21 +0200

    :white_check_mark: Add solution for #4

--- src/0004-median-of-two-sorted-arrays/solution.c
+++ src/0004-median-of-two-sorted-arrays/solution.c
@@ -6,7 +6,29 @@
 
 double find_median_sorted_arrays(const int *a, int a_size, const int *b, int b_size)
 {
-    return 0;
+    int len = a_size + b_size;
+    int i = 0;
+    int j = 0;
+    double res;
+    bool odd = len & 1;
+
+    for (int k = 0; k < (len >> 1) - !odd; k++) {
+        if (i == a_size || (j != b_size && a[i] > b[j]))
+            j++;
+        else
+            i++;
+    }
+    if (i == a_size || (j != b_size && a[i] > b[j]))
+        res = b[j++];
+    else
+        res = a[i++];
+    if (odd)
+        return res;
+    if (i == a_size || (j != b_size && a[i] > b[j]))
+        res += b[j++];
+    else
+        res += a[i++];
+    return res / 2;
 }
 
 int findMedianSortedArrays() USED ALIAS("find_median_sorted_arrays");
