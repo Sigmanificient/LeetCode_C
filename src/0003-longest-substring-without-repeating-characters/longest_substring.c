2a35161467b9739c6e9d74f9028dfafb53e0b451
Sigmanificient <edhyjox@gmail.com>
2023-07-05 11:56:04 +0200

    :recycle: Improve my solution using simpler hashmap array

--- src/0003-longest-substring-without-repeating-characters/longest_substring.c
+++ src/0003-longest-substring-without-repeating-characters/longest_substring.c
@@ -1,50 +1,28 @@
 #include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-#define MAX(x, y) ((x > y) ? (x) : (y))
+#include <limits.h>
 
 #define ATTR(key) __attribute__((key))
 #define ALIAS(name) ATTR(alias(name))
 #define USED ATTR(used)
 
-enum { SENTINEL = -1 };
-
-int uniq_chain(const char *str, size_t len, char *pool)
-{
-    bool found;
-    int count = 0;
-    size_t pool_last = 0;
-
-    memset(pool, 0, len);
-    for (size_t i = 0; str[i]; i++) {
-        found = false;
-        for (size_t j = 0; j < pool_last; j++) {
-            if (pool[j] == str[i]) {
-                found = true;
-                break;
-            }
-        }
-        if (found)
-            return count;
-        pool[pool_last++] = str[i];
-        count++;
-    }
-    return count;
-}
-
 int length_of_longest_substring(char *str)
 {
-    int count = 0;
-    size_t len = strlen(str);
-    char *pool = malloc(len * sizeof (char));
+    int left = -1;
+    int longest = 0;
+    int len = (int)strlen(str);
+    int hashmap[CHAR_MAX];
+    int *index;
 
-    if (pool == NULL)
-        return SENTINEL;
-    for (size_t i = 0; i < len; i++)
-        count = MAX(count, uniq_chain(str + i, len - i, pool));
-    free(pool);
-    return count;
+    memset(hashmap, -1, CHAR_MAX * sizeof(int));
+    for (int i = 0; i < len; i++) {
+        index = hashmap + (unsigned char)str[i];
+        if (*index > left)
+            left = *index;
+        *index = i;
+        if (i - left > longest)
+            longest = i - left;
+    }
+    return longest;
 }
 
 int lengthOfLongestSubstring() USED ALIAS("length_of_longest_substring");
